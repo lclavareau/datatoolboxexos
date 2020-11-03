4ef99502c9e724ea5991c7c9951a69e910e2d145
lclavareau <73823810+lclavareau@users.noreply.github.com>
2020-11-03 16:21:35 +0100

    :boom: + exo_dplyr.Rmd/md/html

---
+++ compendium.R
@@ -0,0 +1,8 @@
+
+library(datatoolboxexos)
+datatoolboxexos::data_ecoregion()
+?datatoolboxexos::data_ecoregion()
+devtools::check()
+d <-datatoolboxexos::data_ecoregion()
+head(d)
+devtools::load_all()
